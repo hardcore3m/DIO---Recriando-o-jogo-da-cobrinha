d1f82206a246216f96e58061a94efcf8f871e851
Rafael Marins <rafaelcunha@email.com>
2021-01-11 22:35:54 -0300

    Definindo Movimentaçao da cobra

--- js/main.js
+++ js/main.js
@@ -27,8 +27,25 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+//Chama o evento ao clicar no botao
+document.addEventListener('keydown', update);
+
+/*Mapeia da direção da cobra com os botoes e faz com que ela nao vá ao contrario
+informando que a direção não pode ser oposta*/
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
 
 function iniciarJogo() {
+    /*Plano Cartesiano faz com que a cobra reapareça do outro lado.
+    Quando chega no ponto zero do plano cartesiano ela inicia no 16 e vice-versa*/
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 15;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 15;
     criarBG();
     criarCobrinha();
 
