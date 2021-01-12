40733608a46ea633e5da57750f2836f9cce96fe0
Rafael Marins <rafaelcunha@email.com>
2021-01-11 21:56:35 -0300

    Criando o movimento

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ snake[0] = {
     y: 8 * box
 
 }
+//Direção da cobrinha
+let direction = "right";
 
 function criarBG() {
     //Preenche com estilo, no caso bg verde
@@ -16,6 +18,7 @@ function criarBG() {
     //Fillrect - Desenha onde vai acontecer o evento Parâmetros (posição X, posição Y, altura, largura)
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+//Atualiza o jogo de tempos em tempos
 
 function criarCobrinha() {
     //percorre todo o array incrementando
@@ -24,5 +27,29 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
-criarCobrinha();
+
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    //retira o último elemento do array
+    snake.pop();
+    //acrescenta elemento a frente
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+
+}
+
+//A cada 100 milisegundos a iniciar jogo é renovada para dar continuidade ao jogo
+let jogo = setInterval(iniciarJogo, 100);
